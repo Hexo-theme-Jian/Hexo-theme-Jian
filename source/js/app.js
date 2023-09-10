a5d264f7b467d6a9057334863fcae836129a682a
Fgaoxing <92655031+Fgaoxing@users.noreply.github.com>
2023-09-10 19:13:11 +0800

    Add files via upload

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,6 @@
 Jian = {
     debug: true, // 调试开关
+    var: {},
     console: { // 输出到控制台
         success: (m) => {
             /*
@@ -7,8 +8,8 @@ Jian = {
             @param  m all
             @return undefined
             */
-            if (Jian.debug){ // 判断是否调试
-                console.log(`%c${m}`, 'border-left: 5px solid green;box-shadow: 0 0 32px 0 #79797951;text-decoration: none;border-radius: 3px;color:white !important;background:black;padding: 3px;');
+            if (Jian.debug) { // 判断是否调试
+                console.log(`%c${m}`, 'border-left: 5px solid green;text-decoration: none;border-radius: 3px;color:#000 !important;background:write;padding: 3px');
             }
         }, warning: (m) => {
             /*
@@ -16,20 +17,20 @@ Jian = {
             @param  m all
             @return undefined
             */
-            if (Jian.debug){
-                console.log(`%c${m}`, 'border-left: 5px solid yellow;box-shadow: 0 0 32px 0 #79797951;text-decoration: none;border-radius: 3px;color:white !important;background:black;padding: 3px;');
+            if (Jian.debug) {
+                console.log(`%c${m}`, 'border-left: 5px solid yellow;text-decoration: none;border-radius: 3px;color:#000 !important;background:write;padding: 3px');
             }
         }, info: (m) => {
-            if (Jian.debug){
-                console.log(`%c${m}`, 'border-left: 5px solid dodgerblue;box-shadow: 0 0 32px 0 #79797951;text-decoration: none;border-radius: 3px;color:white !important;background:black;padding: 3px;');
+            if (Jian.debug) {
+                console.log(`%c${m}`, 'border-left: 5px solid dodgerblue;text-decoration: none;border-radius: 3px;color:#000 !important;background:write;padding: 3px');
             }
         }, error: (m) => {
-            if (Jian.debug){
-                console.log(`%c${m}`, 'border-left: 5px solid red;box-shadow: 0 0 32px 0 #79797951;text-decoration: none;border-radius: 3px;color:white !important;background:black;padding: 3px;');
+            if (Jian.debug) {
+                console.log(`%c${m}`, 'border-left: 5px solid red;text-decoration: none;border-radius: 3px;color:#000 !important;background:write;padding: 3px');
             }
         }, debug: (m) => {
-            if (Jian.debug){
-                console.log(`%c${m}`, 'border-left: 5px solid gray;box-shadow: 0 0 32px 0 #79797951;text-decoration: none;border-radius: 3px;color:white !important;background:black;padding: 3px;');
+            if (Jian.debug) {
+                console.log(`%c${m}`, 'border-left: 5px solid gray;text-decoration: none;border-radius: 3px;color:#000 !important;background:write;padding: 3px');
             }
         }, logo: () => {
             console.log(`%c     _ _             \n    | (_) __ _ _ __  \n _  | | |/ _\\\` | '_ \\\n| |_| | | (_| | | | |\n \\___/|_|\\__,_|_| |_|`, "color:white;!important;background:dodgerblue;padding: 3px;text-align: center;")
@@ -49,7 +50,7 @@ Jian = {
                         img.onload = function () {
                             item.src = img.src;
                             Jian.console.success(img.src + ' 加载成功');
-                            let Event = new CustomEvent('Jian:lazyload:load', {detail: {url: img.src, dom: item}})
+                            let Event = new CustomEvent('Jian:lazyload:load', { detail: { url: img.src, dom: item } })
                             document.dispatchEvent(Event);
                             window.dispatchEvent(Event);
                         }
@@ -72,20 +73,25 @@ Jian = {
             if (document.body.classList.value.indexOf('read') !== 0) {
                 document.body.classList.add('read');
                 document.body.addEventListener('click', Jian.plugins.read);
-                let Event = new CustomEvent('Jian:read', {detail: true})
+                let Event = new CustomEvent('Jian:read', { detail: true })
                 document.dispatchEvent(Event);
                 window.dispatchEvent(Event);
             } else {
                 document.body.removeEventListener('click', Jian.plugins.read);
                 document.body.classList.remove('read');
-                let Event = new CustomEvent('Jian:read', {detail: false})
+                let Event = new CustomEvent('Jian:read', { detail: false })
                 document.dispatchEvent(Event);
                 window.dispatchEvent(Event);
             }
         }
     }, onload: {
         list: [], // 加载列表
-        state: false, add: function (fn, ...e) {
+        state: false,
+        Promise: function (fn, ...e) {
+            return new Promise(function (r) {
+                r(fn(...e))
+            })
+        }, add: function (fn, ...e) {
             /*
             用于增加加载列表
             fn：运行的函数
@@ -95,42 +101,43 @@ Jian = {
                 Jian.console.error("onload function must have name(string), f(functicon)");
             }
             if (this.state) {
-                try{
+                try {
                     fn(...e);
-                } catch(e) {
+                } catch (e) {
                     Jian.console.error(e);
                 }
             } else {
-                this.list.push({f: fn, e: e});
+                this.list.push({ fn: fn, e: e })
             }
-            let Event = new CustomEvent('Jian:add_onload', {detail: {fn: fn, e: e}})
+            let Event = new CustomEvent('Jian:add_onload', { detail: { fn: fn, e: e } })
             document.dispatchEvent(Event);
             window.dispatchEvent(Event);
-        }, run: function () {
+        }, run: function (onload = false) {
             /*
             运行加载列表中的函数
             */
+            if (this.state) {return 0;}
             this.state = true;
-            for (var name in this.list) {
-                try{
-                    this.list[name].f(...this.list[name].e);
-                } catch(e) {
+            for (let i = 0; i < this.list.length; i++) {
+                try {
+                    this.Promise(this.list[i].fn, ...this.list[i].e).then();
+                } catch (e) {
                     Jian.console.error(e);
                 }
-                delete this.list[name]
             }
-            let Event = new CustomEvent('Jian:onload')
+            let Event = new CustomEvent('Jian:onload', { detail: { list: this.list } })
             document.dispatchEvent(Event);
             window.dispatchEvent(Event);
         }
-    }, dark: {
+    }
+    , dark: {
         set: function (n) {
             Jian.console.info((n) ? '切换为暗色模式' : '切换为亮色模式');
             if (typeof n !== 'boolean') {
                 Jian.console.error('set(n),n must be a boolean');
             }
-            Jian_var.dark = n
-            let Event = new CustomEvent('Jian:dark_set', {detail: Jian_var.dark})
+            Jian.var.dark = n;
+            let Event = new CustomEvent('Jian:dark_set', { detail: Jian.var.dark })
             document.dispatchEvent(Event);
             window.dispatchEvent(Event);
             localStorage.setItem("dark", n)
@@ -140,7 +147,7 @@ Jian = {
                 document.body.classList.remove('dark');
             }
         }, change: function () {
-            if (Jian_var.dark) {
+            if (Jian.var.dark) {
                 this.set(false);
             } else {
                 this.set(true);
@@ -151,7 +158,7 @@ Jian = {
             Jian.onload.add(function (uri, fn) {
                 var script = document.createElement('script'), fn = fn || function () {
                 };
-                window.dispatchEvent(new CustomEvent('Jian:onload_js', {detail: {url: uri, fn: fn}}));
+                window.dispatchEvent(new CustomEvent('Jian:onload_js', { detail: { url: uri, fn: fn } }));
                 script.type = 'text/javascript';
                 script.onload = function () {
                     Jian.console.success(uri + ' 加载成功')
@@ -168,7 +175,7 @@ Jian = {
             Jian.onload.add(function (uri, fn = undefined) {
                 var css = document.createElement('link'), fn = fn || function () {
                 };
-                window.dispatchEvent(new CustomEvent('Jian:onload_css', {detail: {url: uri, fn: fn}}));
+                window.dispatchEvent(new CustomEvent('Jian:onload_css', { detail: { url: uri, fn: fn } }));
                 css.rel = "stylesheet";
                 css.href = uri;
                 css.onload = function () {
@@ -184,15 +191,15 @@ Jian = {
     }, msg: function (obj) {
         document.msg = document.getElementById('msg');
         let id = `MsgCard-${new Date().getTime()}`
-        document.msg.innerHTML = `<div class="card w-full" id="${id}"><div><div class="title"><i class="${obj.icon || ''}" style="${(obj.icon_color) ? 'color:' + obj.icon_color : ''}"></i> ${obj.title || ''}</div><div class="text">${obj.msg || obj.text || ''}</div></div></div>` + document.msg.innerHTML;
+        document.msg.innerHTML = `<div class="card w-full ${(obj.color) ? 'color-' + obj.color + '-full' : ''}" id="${id}"><div><div class="title"><i class="${obj.icon || ''}"></i> ${obj.title || ''}</div><div class="text">${obj.msg || obj.text || ''}</div></div></div>` + document.msg.innerHTML;
         if (typeof obj.click == 'function') {
             document.getElementById(id).addEventListener('click', obj.click);
         }
-        let Event = new CustomEvent('Jian:onmsg', {detail: obj});
+        let Event = new CustomEvent('Jian:onmsg', { detail: obj });
         document.dispatchEvent(Event);
         window.dispatchEvent(Event);
         setTimeout(function (myselfid) {
-            let Event = new CustomEvent('Jian:add_onload', {detail: myselfid})
+            let Event = new CustomEvent('Jian:add_onload', { detail: myselfid })
             document.dispatchEvent(Event);
             window.dispatchEvent(Event);
             document.getElementById(myselfid).remove();
@@ -203,14 +210,27 @@ Jian = {
 
 DOMLoadStartTime = new Date().getTime();
 window.addEventListener('DOMContentLoaded', function () {
-    Jian.console.info('DOM加载完毕，用时' + (new Date().getTime() - DOMLoadStartTime).toString() + 'ms');
+    Jian.console.info('DOM加载完毕, 用时' + (new Date().getTime() - DOMLoadStartTime).toString() + 'ms');
     if (localStorage.getItem("dark") === 'true') {
         Jian.dark.set(true);
     }
     Jian.console.logo();
     Jian.onload.run();
+    if (document.documentElement.offsetWidth > 672) {
+        document.getElementsByClassName("menu")[0].style.display = "flex"
+    } else {
+        document.getElementsByClassName("menu")[0].style.display = "none"
+    }
 })
 
 if (window.location.pathname[window.location.pathname.length - 1] !== '/' && window.location.pathname.split('/')[0].indexOf('.') === -1 && window.location.pathname !== '/') {
     history.pushState({}, '', window.location.pathname + '/');
 }
+
+window.addEventListener('resize', function () {
+    if (document.documentElement.offsetWidth > 672) {
+        document.getElementsByClassName("menu")[0].style.display = "flex"
+    } else {
+        document.getElementsByClassName("menu")[0].style.display = "none"
+    }
+})
